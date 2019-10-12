bb8595faef5ac61e34c76c9b056f3941fe57b07d
MarceloBonifazio <marcelobonifazio@hotmail.com>
2019-10-12 00:38:53 -0300

    Remove store

--- components/app.js
+++ components/app.js
@@ -1,10 +1,6 @@
 import { LitElement, html, css } from 'lit-element';
 
 class App extends LitElement {
-  static get properties() {
-    // ... properties
-  }
-
   static get styles() {
     return css`div {color: blue}`;
   }
